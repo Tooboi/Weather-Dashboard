d028d513eb77ecf10537e1a7feb584f7cebe1a05
Tooboi <japica123@gmail.com>
2022-11-19 01:39:45 -0500

    API connected

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,39 @@
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
+
+var today = dayjs().format("MMMM D, YYYY h:mm A");
+console.log(today);
+var repoList = document.querySelector("ul");
+var searchButton = document.getElementById("searchBtn");
+var lat = 44;
+var long = 50;
+
+
+function getApi(event) {
+  event.preventDefault();
+  var city = document.getElementById("floatingInput").value;
+  document.getElementById("floatingInput").value = "";
+  window.localStorage.setItem("city", JSON.stringify(city));
+  console.log(city);
+//   var requestCityUrl =
+//     "api.openweathermap.org/geo/1.0/direct?q=" +
+//     city +
+//     "&limit=1&appid=bc8b625028ac837ee20e61a315479c7e";
+  var requestUrl ="https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=bc8b625028ac837ee20e61a315479c7e";
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+    //   for (let i = 0; i < 5.length; i++) {
+    //     const element = 5[i];
+        
+    //   }
+        console.log(data);
+    });
+    }
+//   console.log(requestCityUrl);
+//   console.log(requestUrl);
+searchButton.addEventListener("click", getApi);
